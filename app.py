0874735f0910efdc975078ec41cf2ca1555939e6
Egor Pristavka <pristavkaegor03@gmail.com>
2022-11-16 08:43:24 +0800

    Restored app.py

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
-from flask import Flask, render_template
-from flask import flash, request, redirect, url_for
-import random, os
+from flask import Flask
+from flask import redirect
+from flask import render_template
+from os import getcwd
+
+app = Flask(__name__, template_folder='templates')
+
+CURRENT_ADDRESS = "http://127.0.0.1:5000/"
+
+
+#The page for meme uploading
+@app.route("/uploads",methods=['GET','POST'])
+def upload_meme():
+	return "<h> Welcome to meme uploads page! There is nothing here yet, but soon it will be a cool webpage where you'll can upload new memes! </h>"
+
+#The feed page
+@app.route("/feed", methods=['GET', 'POST'])
+def show_feed():
+	return "<h1>Welcome to da feed page!</h1>"
+
+#Handling of 404 error
+@app.errorhandler(404)
+def page_notexist(e):
+	return render_template('404.html')
 
 #Register page
 @app.route("/register", methods=['POST', 'GET'])
