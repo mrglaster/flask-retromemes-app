a793dee04494b2ed0853929e6882cfa1b8c17e25
Egor Pristavka <pristavkaegor03@gmail.com>
2022-11-22 14:37:16 +0800

    Update rates.py

--- modules/rates.py
+++ modules/rates.py
@@ -4,13 +4,11 @@ from dataclasses import dataclass
 @dataclass
 class Rates:
     id: int
-    real_likes: str
-    real_dislikes: str
+    history_id: int
 
-    def __init__(self, id, real_likes, real_dislikes):
+    def __init__(self, id, history_id):
         self.id = id
-        self.real_dislikes = real_dislikes
-        self.real_likes = real_likes
+        self.history_id = history_id
 
     def __str__(self):
-        return f"( {self.id}, {self.real_likes}, {self.real_dislikes} )"
+        return f"( {self.id}, {self.hostory_id})"
