62a15d98e50cb0ee4c9e10f3a2946b1b1b69afb4
Vladislav <113700660+vladnov138@users.noreply.github.com>
2022-11-28 03:05:28 +0800

    Update constants for ubuntu

--- modules/constants.py
+++ modules/constants.py
@@ -5,10 +5,10 @@ POSTS_TABLENAME = 'post'
 RATES_TABLENAME = 'rates'
 HISTORY_TABLENAME = 'history'
 
-DATABASE_PATH = os.getcwd() + '\\' + "database" + '\\' + "memes.db"
-UPLOAD_FOLDER = os.getcwd() + '\\' + "static\\images\\uploaded_memes\\"
-DEFAULT_AVATAR = os.getcwd() + "\\static\\images\\ava.png"
-UPLOAD_AVATAR_FOLDER = os.getcwd() + '\\static\\images\\avatars\\'
+DATABASE_PATH = os.getcwd() + '/' + "database" + '/' + "memes.db"
+UPLOAD_FOLDER = os.getcwd() + '/' + "static/images/uploaded_memes/"
+DEFAULT_AVATAR = os.getcwd() + "/static/images/ava.png"
+UPLOAD_AVATAR_FOLDER = os.getcwd() + '/static/images/avatars/'
 AVATAR_FOLDER = 'images/avatars/'
 MEMES_FOLDER = 'images/uploaded_memes/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
